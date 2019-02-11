7180d7ec7286e228cdb0b5d091c4931650f6d37d
jaouad <eajaouad@sqli.com>
2019-02-11 13:52:09 +0100

    Updates

---
+++ precache-manifest.622630ce2230f581b74233dedd086b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af368d9d5ac399d10ac9",
+    "url": "/contactmanager_redux/static/js/runtime~main.af368d9d.js"
+  },
+  {
+    "revision": "8502f16afa1eb79fc59d",
+    "url": "/contactmanager_redux/static/js/main.8502f16a.chunk.js"
+  },
+  {
+    "revision": "b4a172ffff3c8fc99020",
+    "url": "/contactmanager_redux/static/js/1.b4a172ff.chunk.js"
+  },
+  {
+    "revision": "8502f16afa1eb79fc59d",
+    "url": "/contactmanager_redux/static/css/main.d3f64128.chunk.css"
+  },
+  {
+    "revision": "b4a172ffff3c8fc99020",
+    "url": "/contactmanager_redux/static/css/1.7abbdc33.chunk.css"
+  },
+  {
+    "revision": "82550987df73e6f1bd8f2d4a6358bb12",
+    "url": "/contactmanager_redux/index.html"
+  }
+];
